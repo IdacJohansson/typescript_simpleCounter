8de07eddf439d34b5f8639dd16d7ebe7b1bfe34e
Ida Johansson <ida.johansson90@hotmail.com>
2025-01-20 15:46:49 +0100

    More types

--- 02week01lesson/app.ts
+++ 02week01lesson/app.ts
@@ -1,31 +1,36 @@
+const numberValue: number = 1;
+const resetValue: number = 0;
+
 // Initial count value
 let count: number = 0;
 
 // Update the count display
-const updateCountDisplay = ():void => {
-    const countElement = document.getElementById('count') as HTMLElement;
-    countElement.textContent = count.toString();
+const updateCountDisplay = (): void => {
+  const countElement = document.getElementById("count") as HTMLElement;
+  countElement.textContent = count.toString();
 };
 
 // Increment the count
-const increment = ():void => {
-    count += 1;
-    updateCountDisplay();
+const increment = (): void => {
+  count += numberValue;
+  updateCountDisplay();
 };
 
 // Decrement the count
-const decrement = ():void => {
-    count -= 1;
-    updateCountDisplay();
+const decrement = (): void => {
+  count -= numberValue;
+  updateCountDisplay();
 };
 
 // Reset the count
-const reset = ():void => {
-    count = 0;
-    updateCountDisplay();
+const reset = (): void => {
+  count = resetValue;
+  updateCountDisplay();
 };
 
 // Attach event listeners to buttons
-(document.getElementById('increment-button') as HTMLElement).onclick = increment;
-(document.getElementById('decrement-button') as HTMLElement).onclick = decrement;
-(document.getElementById('reset-button') as HTMLElement).onclick = reset;
+(document.getElementById("increment-button") as HTMLElement).onclick =
+  increment;
+(document.getElementById("decrement-button") as HTMLElement).onclick =
+  decrement;
+(document.getElementById("reset-button") as HTMLElement).onclick = reset;
